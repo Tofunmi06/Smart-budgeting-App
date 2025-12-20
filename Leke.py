7941d793d3d3c81e19204b1415311225fcc4c9f2
Tofunmi06 <dadatofunmi97@gmail.com>
2025-12-20 18:05:43 +0100

    Add Streamlit budget app files

---
+++ Leke.py
@@ -0,0 +1,15 @@
+def Budget_for_me(X):
+    return{
+        "Needs" : 0.5 * X,
+
+        "Savings" : 0.1 * X,
+
+        "Investment" : 0.15 * X,
+
+        "Wants" : 0.05 * X,
+
+        "Emergency" : 0.1 * X,
+        
+        "Tithe" : 0.1 * X,
+
+    }
